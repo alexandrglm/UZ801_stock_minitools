f3835a6e0dd98c302a54edddb45765d44849059f
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2024-11-06 20:11:52 +0100

    Update functions.js

--- webGUI_new/jetty3/js/functions.js
+++ webGUI_new/jetty3/js/functions.js
@@ -1,6 +1,92 @@
 $(function(){
-    deviceInfo();
+    deviceInfo(); // newer deviceInfo
+
+    setInterval(update, 5000); // init update update 2 from WLAN/MODEM info requestinf and possible modes
+    update();
+    setInterval(update2, 5000);
+    update2();
+    init();
 });
+//
+
+// deviceStatus old engine, renewed
+function init() {
+  const connMode = $.session.get("conn_mode");
+  if (connMode === "1") {
+    $("#manual").prop("selected", true);
+    $("#auto").prop("selected", false);
+  } else { // Auto
+    $("#auto").prop("selected", true);
+    $("#manual").prop("selected", false);
+  }
+}
+
+function update2() {
+  const param = { funcNo: 1002 }; // 1002 WLAN confs, function needs to be linked with newer function wlanSetup() {
+  request(param, (data) => {
+    const flag = data.flag;
+    const errorInfo = data.error_info;
+
+    if (flag === "1") {
+      const result = data.results[0];
+      $("#mifi_ip").text(result.IP);
+      $("#g3_mask").text(result.mask);
+      $("#g3_dns").text(result.dns1);
+      $("#g3_dns2").text(result.dns2);
+      $("#ssid").text(result.ssid);
+      $("#wlan_ip").text(result.wlan_ip);
+      $("#password").text(result.pwd);
+    } else {
+      Alert(errorInfo);
+    }
+  });
+}
+
+function update() {     // TxRx data viewer
+  const param = { funcNo: 1003 };
+  request(param, (data) => {
+    const flag = data.flag;
+    const errorInfo = data.error_info;
+
+    if (flag === "1") {
+      const result = data.results[0];
+      $("#mifi_tx_data").text(parseInt(result.up_bytes) + " KB");
+      $("#mifi_rx_data").text(parseInt(result.down_bytes) + " KB");
+      $("#client_num").text(result.client_num + " / " + result.maxSta);
+    } else {
+      Alert("Fail");
+      Alert(errorInfo);
+    }
+  });
+}
+
+$("#connect").on("change", () => {
+  if ($("#manual").prop("selected")) {
+    $("#manual").prop("selected", true);
+    $("#auto").prop("selected", false);
+  } else {
+    $("#auto").prop("selected", true);
+    $("#manual").prop("selected", false);
+  }
+});
+
+$("#apply").on("click", () => {
+  const connMode = $("#auto").prop("selected") ? $("#auto").val() : $("#manual").val();
+  const param = { funcNo: 1004, conn_mode: connMode }; // la 1004: 3g/4g, #manual needs to get its values defined (umts/lte)
+  request(param, (data) => {
+    const flag = data.flag;
+    const errorInfo = data.error_info;
+
+    if (flag === "1") {
+      $.session.set("conn_mode", connMode);
+      Alert("Success");
+    } else { // Error
+      Alert("Fail");
+      Alert(errorInfo);
+    }
+  });
+});
+
 
 // la deviceInfo() viene de deviceInformation()
 function deviceInfo() {
