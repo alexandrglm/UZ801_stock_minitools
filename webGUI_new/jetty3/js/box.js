7fd6bc20f41fdc7eb3885d0047f06afd740af0ef
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2024-11-06 16:45:32 +0100

    Create box.js

---
+++ webGUI_new/jetty3/js/box.js
@@ -0,0 +1,36 @@
+function deviceInfo {
+    var contentBox = document.getElementById("content-box");
+    contentBox.innerHTML = deviceInfo();
+}
+function wlanSetup {
+    var contentBox = document.getElementById("content-box");
+    contentBox.innerHTML = wlanSetup();
+}
+function lteSIM {
+    var contentBox = document.getElementById("content-box");
+    contentBox.innerHTML = lteSIM();
+}
+
+function androidThings {
+    var contentBox = document.getElementById("content-box");
+    contentBox.innerHTML = androidThings();
+}
+
+function shell {
+    var contentBox = document.getElementById("content-box");
+    contentBox.innerHTML = shell();
+}
+/*
+function deviceInfo {
+    var contentBox = document.getElementById("content-box");
+    contentBox.innerHTML = deviceInfo();
+}
+function deviceInfo {
+    var contentBox = document.getElementById("content-box");
+    contentBox.innerHTML = deviceInfo();
+}
+function deviceInfo {
+    var contentBox = document.getElementById("content-box");
+    contentBox.innerHTML = deviceInfo();
+}
+*/
