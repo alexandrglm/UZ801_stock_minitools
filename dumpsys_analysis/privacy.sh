1448c4da498bdf3a61c422eec517cc4ef258707e
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2024-12-09 20:13:56 +0100

    Update privacy.sh

--- dumpsys_analysis/privacy.sh
+++ dumpsys_analysis/privacy.sh
@@ -1,28 +1,13 @@
 #!/bin/bash  
 # bash shebangs may give errors, try changing
-# the use of find could improves the extension instead modularity/visibility, but it's so fine
+# Using "find" method would improves the extension instead modularity/visibility, but this was fine like this.
 
 findSerials () {
   local text="$1"
-  # awk '/\[ro.serial.no]:/ {serial=$2}; gsub (serial, "PRIVACY_REMOVED" {print $0]}' <<< "$text") 
-  # text=$(awk '/\[ro.serial.no]:/ {serial=$2}; gsub (serial, "PRIVACY_REMOVED" 1' <<< "$text")
   text=$(awk '/\[ro\.serialno\]:/ {serial=$2; gsub(serial, "PRIVACY_REMOVED")} 1' <<< "$text")
 }
 
 privacyLog() {
-#  find ./www/ -name "*.html" -print0 | while IFS= read -r -d $'\0' htmlPath; do
-#    text=$(cat "$htmlPath")
-#    text=$(findSerials "$text")
-#    text=$(echo "$text" | sed -E '
-#      s/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/PRIVACY_REMOVED/g; # ipv4
-#      s/(^|\s)([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}\/[0-9]{1,3}(\s|$)/\1\3/g; #ipv6
-#      s/([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}/PRIVACY_REMOVED/g; # macs
-#      s/\[android-.*\]/PRIVACY_REMOVED/g; # hosts
-#      s/\[ro.serialno]: \[.*\]/PRIVACY_REMOVED/g # serials
-#      ')
-#      echo "$text" > "$htmlpath"
-#    done && echo "Done!!"
-  
   local text="$1"
 
   # IPv4&6
